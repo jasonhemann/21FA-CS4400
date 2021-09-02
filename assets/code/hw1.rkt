286e28d1a6c95475e8f0575eda899f3e890f24e8
Jason Hemann <jason.hemann@gmail.com>
2021-09-02 10:44:17 -0400

    Adding basic start files for hws 1,2, and 3

---
+++ assets/code/hw1.rkt
@@ -0,0 +1,351 @@
+#lang racket
+
+#| Recursion and Higher-order Functional Abstraction |# 
+
+;; Recursion is the root of computation since it trades description
+;; for time. 
+
+;; -- Alan Perlis 
+ 
+#| Assignment Guidelines |#
+
+
+;; In addition to the standard Assignment Guidelines
+;; (https://pages.github.ccs.neu.edu/jhemann/21FA-CS4400/hw/)
+
+;; You should write your solutions without creating explict help
+;; functions. You may, however re-use your solutions to prior
+;; problems.
+
+#| 
+
+0. I've recently updated the course syllabus for this semester. Please
+read through it carefully before beginning the rest of the assignment.
+
+|# 
+
+
+#| 
+
+ 1. Define and test a procedure countdown that takes a natural number
+and returns a list of the natural numbers less than or equal to that
+number, in descending order.
+
+|#
+
+
+
+#| 
+
+2. Define and test a procedure insertR that takes two symbols and a
+list and returns a new list with the second symbol inserted after each
+occurrence of the first symbol. For this and later questions, these
+functions need only hold over eqv?-comparable structures.
+
+|#
+
+
+
+#| 
+
+3. Define and test a procedure remv that takes an atom and a list of
+atoms and returns a list similar except its missing the first
+occurrence (if any) of the input atom.
+
+|# 
+
+
+
+#| 
+
+4. Define and test a procedure list-index-ofv that takes an element
+and a list containing that element and returns the (base 0) index of
+that element in the list. List without that element are bad data.
+
+|# 
+
+
+
+#| 
+
+5. Define and test a procedure filter that takes a predicate and a
+list and returns a new list containing the elements that satisfy the
+predicate. A predicate is a procedure that takes a single argument and
+returns either #t or #f. The number? predicate, for example, returns
+#t if its argument is a number and #f otherwise. The argument
+satisfies the predicate, then, if the predicate returns #t for that
+argument.
+
+|# 
+
+
+
+#| 
+
+6. Define and test a procedure zip that takes two lists and forms a
+new list, each element of which is a pair formed by combining the
+corresponding elements of the two input lists. If the two lists are of
+uneven length, zip will drop the tail of the longer one.
+
+|# 
+
+
+
+#| 
+
+7. Define and test a procedure map that takes a procedure p of one
+argument and a list ls and returns a new list containing the results
+of applying p to the elements of ls. Do not use Racket's built-in map
+in your definition.
+
+|# 
+
+
+
+#| 
+
+8. Define and test a procedure append that takes a list l and any
+racket datum d, and returns a new racket datum with the elements of l
+prepended. This should work for any racket datum d, but testing
+against the data we have talked about in class is sufficient.
+
+|# 
+
+
+
+#| 
+
+9. Define and test a procedure reverse that takes a list l and returns
+list with the elements of l in the opposite order
+
+|# 
+
+
+
+#| 
+
+10. Define and test a procedure fact that takes a natural number and
+computes the factorial of that number. The factorial of a number is
+computed by multiplying it by the factorial of its predecessor. The
+factorial of 0 is defined to be 1 (https://oeis.org/A000142).
+
+|# 
+
+
+
+#|
+
+11. Define and test a procedure fib that takes a natural number n as
+input and computes the nth number, starting from zero, in the
+Fibonacci sequence (0, 1, 1, 2, 3, 5, 8, 13, 21, …). Each number in
+the sequence is computed by adding the two previous numbers.
+
+|# 
+
+
+#| 
+
+12. Define a function cons-every that takes an element x and a list l
+and returns a list with x consed to the front of each element of l. 
+
+|# 
+
+#| 
+
+13. Define and test a procedure binary->natural that takes a flat list
+of 0s and 1s representing an unsigned binary number in reverse bit
+order and returns that number. For example:
+
+|# 
+
+
+
+#| 
+
+14. Define subtraction using natural recursion. Your subtraction
+function, minus, need only take nonnegative inputs where the result
+will be nonnegative.
+
+|# 
+
+
+
+#| 
+
+15. Define division using natural recursion. Your division function,
+div, need only work when the second number evenly divides the
+first (that is, for the divisible abelian group that's a subgroup of
+Nat). Divisions by zero is of course bad data.
+
+|# 
+
+
+
+#| 
+
+16. Define a function append-map that, similar to map, takes both a
+procedure p of one argument a list of inputs ls and applies p to each
+of the elements of ls. Here, though, we mandate that the result of p
+on each element of ls is a list, and we append together the
+intermediate results. Do not use Racket's built-in append-map in your
+definition.
+
+|# 
+
+
+
+#| 
+
+17. Define a function set-difference that takes two flat sets (lists
+with no duplicate elements) s1 and s2 and returns a list containing
+all the elements in s1 that are not in s2.
+
+|# 
+
+
+
+#| Brainteasers |#
+
+#| 
+
+19. In mathematics, the power set of any set S, denoted P(S), is the
+set of all subsets of S, including the empty set and S itself. The
+procedure powerset takes a list and returns the power set of the
+elements in the list. Implementations may different in the exact order
+of their results' sublists.
+
+|# 
+
+
+#| 
+
+20. The cartesian-product is defined over a non-empty list of
+sets (again, by our agreed upon convention, sets are lists that don't
+have duplicates). The result is a list of tuples (i.e. a list of
+lists). Each tuple has in the first position an element of the first
+set, in the second position an element of the second set, etc. The
+output list should contains all such combinations. The exact order of
+your tuples may differ; this is acceptable.
+
+|#
+
+
+#| 
+
+21. Rewrite some of the natural-recursive programs from above instead
+using foldr. That is, the bodies of your definitions should not refer
+to themselves. The names should be as follows.
+
+I recommend this (http://www.cs.nott.ac.uk/~pszgmh/fold.pdf) treatise
+on fold operators. It contains answers to several of the above
+sub-problems. It will also teach you about programming with
+foldr. There are stunningly beautiful definitions of the last two
+sub-problems. They're just mind-blowing. And to tease you further,
+know that some (pretty clever, albeit) folk solved this almost 50
+years ago, back when lexical scope wasn't a thing, higher-order
+functions weren't commonplace like they are today, and many of the
+common programming idioms and that we take for granted just weren't
+around. Since I hate to pass up an excuse to show off something cool,
+I gotta tell you about
+[this](https://www.brics.dk/RS/07/14/BRICS-RS-07-14.pdf) derivation
+and explanation of answers to the last couple of problems, but you
+have to promise (1) you'll try it first on your own, and (2) that if
+you peek at the answers, you'll read the whole thing. It's short,
+moves quickly, and very high enlightenment/text ratio. That's my sales
+pitch.
+
+- insertR-fr
+- filter
+- map-fr
+- append-fr
+- reverse-fr
+- binary->natural-fr
+- append-map-fr
+- set-difference-fr
+- powerset-fr
+- cartesian-product-fr
+
+|# 
+
+#| 
+
+22. Consider the below function f. It is an open question in
+mathematics, known as the Collatz Conjecture, as to whether, for every
+positive integer n, the function power limit of f on n is 1. Your task
+is to, complete the below definition of collatz. collatz should be a
+function which will, when given a positive integer as an input,
+operate in a manner similar to the straightforward recursive function
+f.
+
+Your completed answer should be very short. It must be no more than
+one line long (and prettily-indented, so don't think about squeezing
+something big on a single line), and must not use lambda. Your
+collatz should compute the collatz of positive integers; for
+non-positive integers, it should signal an error “Invalid value”.
+
+|# 
+
+(define (f n)
+  (cond
+    ((even? n) (/ n 2))
+    ((odd? n) (add1 (* n 3)))))
+
+#| 
+
+(define collatz
+  (letrec
+    ((odd-case
+      (lambda (recur)
+        (lambda (x)
+          (cond 
+            ((odd? x) (collatz (add1 (* x 3)))) 
+            (else (recur x))))))
+     (even-case
+      (lambda (recur)
+        (lambda (x)
+          (cond 
+            ((even? x) (collatz (/ x 2))) 
+            (else (recur x))))))
+     (one-case
+      (lambda (recur)
+        (lambda (x)
+          (cond
+            ((zero? (sub1 x)) 1)
+            (else (recur x))))))
+     (base
+      (lambda (x)
+        (error 'error "Invalid value ~s~n" x))))
+    ... ;; this should be a single line, without lambda
+    ))
+
+|# 
+
+#| Just Dessert |# 
+
+#| 
+
+23. A quine is a program whose output is the listings (i.e. source
+code) of the original program. In Racket, 5 and #t are both quines.
+
+> 5
+5
+> #t
+#t
+
+We will call a quine in Racket that is neither a number nor a boolean
+an interesting Racket quine. Below is an interesting Racket quine.
+
+> ((lambda (x) (list x (list 'quote x)))
+  '(lambda (x) (list x (list 'quote x))))
+'((lambda (x) (list x (list 'quote x)))
+   '(lambda (x) (list x (list 'quote x))))
+
+Write your own interesting Racket quine, and define it as quine. The
+following should then be true. Not every Racket list is a quine;
+Racket's standard printing convention will prepend a quote to a
+list. Make sure to use the above tests.
+
+|# 
+
+
+
---
+++ assets/code/hw3.rkt
@@ -0,0 +1,275 @@
+#lang racket
+(require rackunit)
+
+#| Environments and Interpreters |# 
+
+
+;; Inverting the adage that a data type is just a simple programming
+;; language, we take the position that a programming language is,
+;; semantically, just a complex data type; evaluation of a program is
+;; just another operation in the data type.
+
+;; -- Mitch Wand
+ 
+#| Assignment Guidelines |#
+
+;; Recall that in recent lectures, we've learned how to write an
+;; interpreter that takes a Racket expression and returns the
+;; expression's value. We have also learned to make this interpreter
+;; representation independent with respect to environments, and we
+;; have written two different representations of the helpers
+;; extend-env, apply-env, and empty-env.
+
+;; In the first part of this assignment you will implement the three
+;; interpreters I presented in lecture.
+
+;; For the 2nd and 3rd interpreters you must also define two sets of
+;; environment helpers: one that uses functional (higher-order)
+;; representation of environments, and one that uses data-structural
+;; representation of environments. 
+
+;; Your data structure representations should be the tagged list
+;; representation demonstrated in class.
+
+;; You must name your interpreters and helpers for each of the first
+;; three problems respectively by the following naming
+;; conventions. These below names may differ sligtly from the names I
+;; used in lecture.
+
+#| 
+(define value-of ...)
+ 
+(define value-of-fn ...)
+(define empty-env-fn ...)
+(define extend-env-fn ...)
+(define apply-env-fn ...)
+ 
+(define value-of-ds ...)
+(define empty-env-ds ...)
+(define extend-env-ds ...)
+(define apply-env-ds ...)
+|# 
+
+;; Your first three interpreters must all handle the following forms:
+;; numbers, booleans, variables, lambda-abstraction, application,
+;; zero?, sub1, *, if, and let.
+
+;; In the second part you will implement a fourth interpreter, this
+;; time an interpreter for a new language.
+
+;; For this assignment your solutions must be compositional or you
+;; will lose credit.  E.g. although we could rewrite the expression
+;; (let ([x e]) body) as ((lambda (x) body) e), you must not use
+;; lambda in this way for your interpreter's line for let
+;; expressions. Instead, you must implement let in its own right.
+
+#| Interpreters and Environments |#
+
+#|
+
+1. value-of 
+
+|# 
+
+(check-equal?
+ "A nearly-sufficient test-case of your program's functionality"
+ (value-of
+  '(((lambda (f)
+       (lambda (n) (if (zero? n) 1 (* n ((f f) (sub1 n))))))
+     (lambda (f)
+       (lambda (n) (if (zero? n) 1 (* n ((f f) (sub1 n)))))))
+    5)
+  (lambda (y) (error 'value-of "unbound variable ~a" y)))
+ 120)
+
+
+#| 
+
+2. value-of-fn. 
+
+We walked through the steps of implementing this in lecture, at least
+for the basic forms. From a software engineering point-of-view, you
+can see this process as follows. In the above, we hard-coded our
+implementation of "environment", tightly coupling the client
+code (value-of) to the implementation of environments. Now, we will
+correct that mistake by engineering an interface: `apply-env-fn`,
+`extend-env-fn`, and `empty-env-fn` collectively make up the interface
+for environments. By replacing those hard-coded implementations of
+environments with calls to these "help functions" we will construct an
+interface against which we can program, correctly separating the
+client code that uses environment from the implementation that
+provides environment across the interface.
+
+|# 
+
+#| 
+
+3. value-of-ds
+
+We walked through the steps of implementing this in lecture, at least
+for the basic forms. We can also see this step from a software
+engineering point-of-view as follows. In the above, we shimmed in an
+interface to separate our client code that uses environment and our
+implementation code that provides environment, across an interface
+that is the three functions `apply-env-fn`, `extend-env-fn`, and
+`empty-env-fn`. In this step, we will now demonstrate to ourselves
+that this was a well-defined interface, that is, that our interface is
+not "leaky." We will re-implement environment, using an entirely
+different representation behind-the-scenes. Since our client (the
+interpreter) is programming against the interface, though, the client
+code won't have to change at all*.
+
+With this switch to a representation of environments as data
+structures, we notice another neat thing. We changed our environment
+went from a higher-order, functional representation in the last part
+of the assignment to now, a first-order data structure
+representation. And we can match against our environment like you
+would any old data definition. 
+
+
+* Okay, so we're in point of fact changing both the interpreter client
+code and the "helper" interface functions from -fn to -ds, but this is
+just so that we can have the different versions of this interpreter in
+the same file.
+
+|# 
+
+#| A New Syntax |#
+
+#| 
+
+4. Implement an interpreter fo-eulav. Let the below examples guide
+you. I only require you to implement those forms I use in those
+examples.
+
+|# 
+
+(check-equal?
+ "Ppa"
+ (fo-eulav '(5 (x (x) adbmal)) (lambda (y) (error 'fo-eulav "unbound variable ~s" y)))
+ 5) 
+(check-equal?
+ "Stnemugra sa Snoitcnuf"
+ (fo-eulav '(((x 1bus) (x) adbmal) ((5 f) (f) adbmal)) (lambda (y) (error 'fo-eulav "unbound variable ~s" y)))
+ 4) 
+(check-equal?
+ "Tcaf"
+ (fo-eulav
+  '(5  
+    (((((((n 1bus) (f f)) n *) 1 (n ?orez) fi)
+       (n) adbmal)
+      (f) adbmal)
+     ((((((n 1bus) (f f)) n *) 1 (n ?orez) fi)
+       (n) adbmal)
+      (f) adbmal))) 
+  (lambda (y) (error 'fo-eulav "unbound variable ~s" y)))
+ 120)
+
+#| Brainteasers |#
+
+;; Consider the following interpreter for a deBruijnized version of
+;; the lambda-calculus (i.e. lambda-calculus expressions using lexical
+;; addresses addresses instead of variables). Notice this interpreter
+;; is representation-independent with respect to environments. There
+;; are a few other slight variations in the syntax of the
+;; language. These are of no particular consequence.
+
+(define (value-of-lex exp env)
+  (match exp
+    [`(const ,expr) expr]
+    [`(mult ,x1 ,x2) (* (value-of-lex x1 env) (value-of-lex x2 env))]
+    [`(zero ,x) (zero? (value-of-lex x env))]
+    [`(sub1 ,body) (sub1 (value-of-lex body env))]
+    [`(if ,t ,c ,a) (if (value-of-lex t env) (value-of-lex c env) (value-of-lex a env))]
+    [`(var ,num) (apply-env-lex env num)]
+    [`(lambda ,body) (lambda (a) (value-of-lex body (extend-env-lex a env)))]
+    [`(,rator ,rand) ((value-of-lex rator env) (value-of-lex rand env))]))
+ 
+(define (empty-env-lex)
+  '())
+
+(check-equal?
+ "This test shows we're using a data-structure representation of environments."
+ (value-of-lex '((lambda (var 0)) (const 5)) (empty-env-lex))
+ 5)
+
+#| 
+
+5. Without using lambda or the implicit lambda in an "MIT-define",
+define apply-env-lex and extend-env-lex. A correct solution is very
+short.  
+
+|#
+
+#|
+
+6. Go back and extend your interpreter value-of to support set! and
+begin2, where begin2 is a variant of Racket's begin that takes exactly
+two arguments, and set! mutates variables.
+
+|# 
+
+#| Just Dessert |# 
+
+;; The lambda calculus can be used to define a representation of
+;; natural numbers, called Church numerals, and arithmetic over
+;; them. For instance, c5 is the definition of the Church numeral for
+;; 5. This is often described as "representing a number by its
+;; fold". What they mean by this is: think of any given number not a
+;; piece of data, but in terms of "the interface it implements." What
+;; does a number *do* for you? It tells you how many times to iterate
+;; some behavior. 
+
+
+
+(define c0 (lambda (s) (lambda (z) z)))
+(define c5 (lambda (s) (lambda (z) (s (s (s (s (s z))))))))
+
+(check-equal?
+ "Church 5 acts like 5"
+ ((c5 add1) 0)
+ 5)
+
+(check-equal?
+ "Church 0 acts like 0"
+ ((c0 add1) 0)
+ 0)
+
+;; The following is a definition for Church plus, which performs
+;; addition over Church numerals.
+
+(define c+
+  (lambda (m) 
+    (lambda (n) 
+      (lambda (s)
+        (lambda (z)
+          ((m s) ((n s) z)))))))
+
+
+(define c10 ((c+ c5) c5))
+
+(check-equal?
+ "Church addition acts like addition on Church numerals"
+ ((c10 add1) 0)
+ 10)
+
+;; One way to understand the definition of c+ is that it, when
+;; provided two Church numerals, returns a function that, when
+;; provided a meaning for add1 and a meaning for zero, uses provides
+;; to m the meaning for add1 and, instead of the meaning for zero,
+;; provides it the meaning for its second argument. m is the sort of
+;; thing that will count up m times, so the resulting function is the
+;; meaning of m + n.
+
+#| 
+
+7. Your task, however, is to implement csub1, Church predecessor. You
+should also provide tests. The Church predecessor of Church zero is
+zero, as we haven't a notion of negative numbers. This was a difficult
+problem, but it's fun, so don't Google it. If you think it might help
+though, consider taking a [trip to the
+dentist](http://link.springer.com/chapter/10.1007%2FBFb0062850).
+
+|#
+
+
