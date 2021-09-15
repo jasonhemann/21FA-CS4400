675d446fbe2c98757f3d21d23f5a1435726c2c06
Jason Hemann <jason.hemann@gmail.com>
2021-09-15 11:31:17 -0400

    Pushing up changes to variable reference description in start file

--- assets/code/hw2.rkt
+++ assets/code/hw2.rkt
@@ -29,17 +29,15 @@
 ;; Most of section 2's problems require both match and recursion on
 ;; lambda-calculus expressions.
 
+;; You will want each of these problems to be structurally recursive. 
+
 ;; You may find match also simplifies other problems.
 
 ;; The match features we demonstrated in class are sufficient. Do not
 ;; use features of match we did not discuss in class.
 
-;; Here and forevermore in this class we use the word occur in its
-;; technical sense: for us, a formal parameter does not count as a
-;; variable occurrence.
-
-;; Yes, a variable can both occur free and occur bound in the same
-;; expression. 
+;; Yes, the same expression expression can contain both a free
+;; reference to `x` and a bound reference to `x`.
 
 ;; In a subset of Racket where lambdas have only one argument, the
 ;; lexical address of a variable is the number of lambdas between the
@@ -68,10 +66,10 @@ function. It should behave like Racket's list-ref.
 
 (define (list-ref ls n)
   (letrec
-    ([nth-cdr
-      (λ (n)
-        ;; complete the definition
-        )])
+      ([nth-cdr
+        (λ (n)
+          ;; complete the definition
+          )])
     (car (nth-cdr n))))
 
 ;; The inner, nested function nth-cdr needs its body. Complete the
@@ -79,7 +77,9 @@ function. It should behave like Racket's list-ref.
 ;; implementation of nth-cdr, so that the following tests pass. You
 ;; must not modify the provided code beyond adding a body. You may of
 ;; course add newlines as needed. Do not call list-ref, either ours or
-;; Racket's, in your definition.
+;; Racket's, in your definition. Follow our pattern: figure out the
+;; base case, write the natural recursion, and figure out what to do
+;; with the result of the recursion.
 
 #|
 
@@ -87,7 +87,7 @@ function. It should behave like Racket's list-ref.
 neither list contains the same element twice (that is, the list
 represents a set), and returns a list containing the union of the two
 input lists. Again, the order of the elements in your answer does not
-matter. You should use memv on this problem. 
+matter. You should use `memv` on this problem. 
 
 |# 
 
@@ -104,12 +104,15 @@ to x or satisfy pred.
 
 #| 
 
-4. Define and test a procedure walk-symbol that takes an atom x and an
-association list of symbols to atoms. Your procedure should search
-through s for the value associated with x. If the associated value is
-a symbol, it too must be walked in s. If x has no association, then
-walk-symbol should return x. You should use assv on this
-problem. Cycles are absolutely bad data.
+4. Define and test a procedure walk-symbol that takes a Racket datum
+and an association list of symbols to Racket data. Your procedure
+should search through s for the value associated with x. If the
+associated value is a symbol, it too must be walked in s. If x has no
+association, then walk-symbol should return x. You should use `assv`
+on this problem. Cycles are absolutely bad data.
+
+> (walk-symbol 'd '((a . 5) (b . (1 2)) (c . a) (e . c) (d . e)))
+5
 
 |# 
 
@@ -123,17 +126,17 @@ problem. Cycles are absolutely bad data.
 5. Define and test a procedure lambda->lumbda that takes a
 lambda-calculus expression and returns the expression unchanged with
 the exception that each lambda as a keyword has been replaced with the
-word lumbda (notice you should not change occurrences of lambda in
-variable position).
+word lumbda (notice you should not change declarations of a variable
+`lambda`).
 
 |# 
 
 
 #| 
 
-6. Define and test a procedure var-occurs? that takes a variable name
-and a lambda-calculus expression and returns a boolean answering
-whether that variable occurs in the expression. 
+6. Define and test a procedure reference-occurs? that takes a variable
+name and a lambda-calculus expression and returns a boolean answering
+whether the expression contains a reference to that variable.
 
 |# 
 
@@ -141,10 +144,10 @@ whether that variable occurs in the expression.
 #| 
 
 7. Define and test a procedure vars that takes a lambda-calculus
-expression and returns a list containing all variables that occur in
-the expression. This should be a straightforward modification of
-lambda->lumbda, and the order of the variables in your answer does not
-matter.
+expression and returns a list containing all the variable references
+(the variable name itself) in the expression. This should be a
+straightforward modification of lambda->lumbda, and the order of the
+variables in your answer does not matter. 
 
 |#
 
@@ -152,28 +155,30 @@ matter.
 #| 
 
 8. Define and test a function called unique-vars that behaves like
-vars but does not return duplicates. Use union in your definition.
+vars but returns a set---i.e. it eliminates duplicates as it
+evaluates. Use union in your definition.
 
 |#
 
 
 #| 
 
-9. Define and test a procedure var-occurs-free? that takes a symbol
-and a lambda-calculus expression and returns #t if that variable
-occurs free in that expression, and #f otherwise. You may have seen
-solutions in class to these problems that use accumulators. You are
-not permitted to use accumulators on these problems; such solutions do
-not receive credit.
+9. Define and test a procedure free-reference-occurs? that takes a
+symbol and a lambda-calculus expression and returns #t if that that
+expression contains a free reference to that variable, and #f
+otherwise. You may have seen solutions in class to these problems that
+use accumulators. You are not permitted to use accumulators on these
+problems; such solutions do not receive credit.
 
 |#
 
 
 #| 
 
-10. Define and test a procedure var-occurs-bound? that takes a symbol
-and a lambda-calculus expression and returns #t if that variable
-occurs bound in the expression, and #f otherwise.
+10. Define and test a procedure bound-reference-occurs? that takes a
+symbol and a lambda-calculus expression and returns #t if that
+expression contains a bound reference to the variable, and #f
+otherwise.
 
 |# 
 
@@ -181,10 +186,11 @@ occurs bound in the expression, and #f otherwise.
 #| 
 
 11. Define and test a procedure unique-free-vars that takes a
-lambda-calculus expression and returns a list of all the variables
-that occur free in that expression. Order doesn't matter, but the list
-must not contain duplicate variables. You may find it helpful to use
-the definition of unique-vars as a starting point.
+lambda-calculus expression and returns a set (represented as a list)
+of all the free variable references in that expression. Order doesn't
+matter, but the list must not contain duplicate variables. You may
+find it helpful to use the definition of unique-vars as a starting
+point.
 
 |#
 
@@ -198,9 +204,9 @@ the definition of unique-vars as a starting point.
 #| 
 
 12. Define and test a procedure unique-bound-vars that takes a
-lambda-calculus expression and returns a list of all the variables
-that occur bound in the input expression. Order doesn't matter, but
-the list must not contain duplicate variables.
+lambda-calculus expression and returns a set (represented as a list)
+of all the bound variable references in the input expression. Order
+doesn't matter, but the list must not contain duplicate variables.
 
 |# 
 
@@ -212,15 +218,15 @@ the list must not contain duplicate variables.
 expression and an accumulator (which starts as the empty list), and
 returns the same expression with all bound variable references
 replaced by lists of two elements whose car is the symbol var and
-whose cadr is the lexical address of the referenced
-variable. Expressions with free variables are bad data. 
+whose cadr is the lexical address of the referenced variable. For this
+problem expressions with free variable references are bad data.
 
 This problem has several good solutions. I suggest you start by
 building some lambda expressions on paper and then, by hand, find the
-lexical addresses of some variables that occur in them. Try and do it
-almost mechanically, starting from the top of the expression and
-working your way down. Then think about what it is you're doing, and
-try and figure out how to do it without having to go back up the
+lexical addresses of some variable references that occur in them. Try
+and do it almost mechanically, starting from the top of the expression
+and working your way down. Then think about what it is you're doing,
+and try and figure out how to do it without having to go back up the
 tree. That is, ensure that when you get to a variable position in the
 expression where you need to fill in the lexical address, that you
 already have all the information you need to figure it out. Then code
@@ -290,18 +296,16 @@ example:
 ;; documentation on values and let-values (and call-with-values though
 ;; you probably won't need it to define var-occurs-both?).
 
-;; I use the chk library for testing here because it copes better with
-;; values
-
 #| 
 
-15. Define a predicate var-occurs-both? that takes a variable x and a
-lambda-calculus expression, and returns two values, the first of which
-is a boolean answering whether the variable occurs free in the
-expression, and the second is a boolean answering whether the var
-occurs bound in the expression. Your solution should be a one-pass
-solution, meaning you should not recur over the same data twice, and
-you should not use an accumulator. 
+15. Define a predicate free-and-bound-references-occur? that takes a
+variable x and a lambda-calculus expression, and returns two values,
+the first of which is a boolean answering whether the a free reference
+to x occurs in the expression, and the second is a boolean answering
+whether the a bound variable reference to x occurs bound in the
+expression. Your solution should be a one-pass solution, meaning you
+should not recur over the same data twice, and you should not use an
+accumulator.
 
 |# 
 
